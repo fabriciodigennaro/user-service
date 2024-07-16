a4636076a920f35ca8b637c4546b703644d293fe
fabriciodigennaro <slither33f@gmail.com>
2024-07-16 13:46:57 +0200

    first commit

---
+++ src/test/java/com/example/user_login_service/UserLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_login_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
